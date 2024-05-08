771c0560c67ab7cd5bb44d85b79f50dc9bc5fbda
Евгений Малинин <zamelane@vk.com>
2024-05-08 14:32:57 +0700

    Deploy website - based on c19f61a4ba4ab171bb03b9d3bbf132ffe25c4cd1

---
+++ assets/js/2a088d09.bf9ed0ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksources=self.webpackChunksources||[]).push([[5537],{3394:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/android_lessons/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- assets/js/acecf23e.7095c9e0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksources=self.webpackChunksources||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog"}')}}]);
